7e5b0b854b019c4277bb21060422a9456379de4f
tokutei12 <tokutei12@gmail.com>
2013-02-03 00:35:08 -0500

    testing div in place

--- PandoraToGrooveshark/script.js
+++ PandoraToGrooveshark/script.js
@@ -1,1 +1,23 @@
-console.log("...");
+//var jq = document.createElement('script');
+//jq.src = "http://code.jquery.com/jquery-latest.min.js";
+//document.getElementsByTagName('head')[0].appendChild(jq);
+//jQuery.noConflict();
+//function (a,b){return new e.fn.init(a,b,h)}
+
+var gsButton=document.createElement("div");
+var textnode=document.createTextNode("TESTING");
+gsButton.appendChild(textnode);
+gsButton.setAttribute("style", "margin: 6px 3px 0 3px; float:left;");
+gsButton.onclick=function(){console.log("did something")};
+
+var playbackControl=document.getElementById("playbackControl");
+var buttondiv = null;
+for (var i = 0; i < playbackControl.childNodes.length; i++) {
+    if (playbackControl.childNodes[i].className == "buttons") {
+      buttondiv = playbackControl.childNodes[i];
+      break;
+    }        
+}
+if(buttondiv!=null){
+buttondiv.appendChild(gsButton);
+}
